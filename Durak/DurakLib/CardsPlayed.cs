cd0fff1fb7da8b23784108636dc6e83cd504c944
chaos6631 <chriscalder@rogers.com>
2017-03-23 15:16:49 -0400

    added cardsplayed properties

--- Durak/DurakLib/CardsPlayed.cs
+++ Durak/DurakLib/CardsPlayed.cs
@@ -9,6 +9,24 @@ namespace DurakGameLib
 {
     class CardsPlayed : Cards
     {
+        /// <summary>
+        /// to be used by aiplayer to make decisions.
+        /// this should be set when a human player plays a card, and it is added to the CardsPlayed object
+        /// </summary>
+        Card humanLastCardPlayed;        
+        
 
+        public Card HumanLastCardPlayed
+        {
+            get
+            {
+                return humanLastCardPlayed;
+            }
+
+            set
+            {
+                humanLastCardPlayed = value;
+            }
+        }
     }
 }
